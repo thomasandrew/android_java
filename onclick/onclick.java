8c8471ac98766b1186f96adf206b8d311d3393bf
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-01 15:05:37 -0300

    Create onclick.java

---
+++ onclick/onclick.java
@@ -0,0 +1,24 @@
+package com.example.my_first_java_for_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TextView txtHello = findViewById(R.id.txtMessage);
+        //txtHello.setText("Hello");
+    }
+
+    public void onBtnClick(View view) {
+        TextView txtHello = findViewById(R.id.txtMessage);
+        txtHello.setText("Hello");
+    }
+}
