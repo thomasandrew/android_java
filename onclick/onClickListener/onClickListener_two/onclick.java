cea48ee49c9d174a66799257f47795ddc1ec2166
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-04 13:44:25 -0300

    Create onclick.java

---
+++ onclick/onClickListener/onClickListener_two/onclick.java
@@ -0,0 +1,34 @@
+package com.example.my_first_java_for_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnHello:
+                //System.out.println("Hello");
+                Toast.makeText(this, "Hello Button Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
+    }
+}
