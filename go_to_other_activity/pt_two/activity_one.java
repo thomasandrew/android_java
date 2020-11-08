9974a3c1c8a5145664d5a17b09d29a92600531d4
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-07 21:15:50 -0300

    Create activity_one.java

---
+++ go_to_other_activity/pt_two/activity_one.java
@@ -0,0 +1,37 @@
+package com.example.my_first_java_for_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("texto", "Este texto veio da primeira activity");
+        startActivity(intent);
+    }
+}
