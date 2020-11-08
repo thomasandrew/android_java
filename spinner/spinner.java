b85c70c3efd785ad8de8ae6116821aecc151271f
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-07 21:54:42 -0300

    Create spinner.java

---
+++ spinner/spinner.java
@@ -0,0 +1,80 @@
+package com.example.my_first_java_for_android;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView citiesList;
+    private Spinner studentsSpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // create spinner
+        studentsSpinner = findViewById(R.id.studentsSpinner);
+
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Meisam");
+        students.add("Brad");
+        students.add("Sarah");
+        students.add("Madeline");
+        students.add("Tom");
+
+        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                students
+        );
+
+        studentsSpinner.setAdapter(studentsAdapter);
+
+        // Create a list
+        citiesList = findViewById(R.id.citiesList);
+
+        final ArrayList<String> cities = new ArrayList<>();
+        cities.add("Zurich");
+        cities.add("New York");
+        cities.add("Berlin");
+        cities.add("Moscow");
+        cities.add("Madrid");
+
+        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                cities
+        );
+
+        citiesList.setAdapter(citiesAdapter);
+
+        // To click
+        citiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, cities.get(position) + " Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
