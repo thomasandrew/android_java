40ab525d671abf18c2df8925b238363655dda7bc
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-11 14:24:21 -0300

    Create CustumerModel.java

---
+++ back_end/java/CustumerModel.java
@@ -0,0 +1,65 @@
+package com.example.back_end;
+
+public class CustumerModl {
+
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    // constructor
+    public CustumerModl(int id, String name, int age, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    public CustumerModl() {
+
+    }
+
+    // To string is necessary for printing contents of a class object
+    @Override
+    public String toString() {
+        return "CustumerModl{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
