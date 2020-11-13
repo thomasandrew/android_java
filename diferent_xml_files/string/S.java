0e31e985b786acfa1546e79d72b31ec62aa9b6ce
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-13 12:40:57 -0300

    Create S.java

---
+++ diferent_xml_files/string/S.java
@@ -0,0 +1,38 @@
+package com.example.my_first_java_for_android;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHello = findViewById(R.id.txtHello);
+        txtHello.setText(R.string.hello);
+    }
+}
