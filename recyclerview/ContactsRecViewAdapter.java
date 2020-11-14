c64831e50c2c8a5f0ae115a230f623201a782ee4
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-14 15:53:05 -0300

    Create ContactsRecViewAdapter.java

---
+++ recyclerview/ContactsRecViewAdapter.java
@@ -0,0 +1,54 @@
+package com.example.my_first_java_for_android;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecViewAdapter.ViewHolder> {
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public ContactsRecViewAdapter() {
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false);
+        //View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, null);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtName.setText(contacts.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts.size();
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtName = itemView.findViewById(R.id.txtName);
+        }
+    }
+}
