b6d259a91c4a6b44335df56f9dc29aa5c544c703
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-11-14 21:49:58 -0300

    Create MainActivity.java

---
+++ font/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.my_first_java_for_android;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
+    private Button btnChangeFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHello = findViewById(R.id.txtHello);
+        btnChangeFont = findViewById(R.id.btnChangeFont);
+
+        final Typeface typeface = ResourcesCompat.getFont(this, R.font.anton);
+
+        btnChangeFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHello.setTypeface(typeface);
+            }
+        });
+    }
+}
