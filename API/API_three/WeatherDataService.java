c1cd92ee5f1b2d99545ba39ded6fb7094ad80e65
Thomas <53497553+thomasandrew@users.noreply.github.com>
2021-05-14 19:38:49 -0300

    Create WeatherDataService.java

---
+++ API/API_three/WeatherDataService.java
@@ -0,0 +1,75 @@
+package com.example.weatherapiapp;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class WeatherDataService {
+
+    public static final String QUERY_FOR_CITY_ID = "https://www.metaweather.com/api/location/search/?query=";
+
+    Context context;
+    String cityID;
+
+    public WeatherDataService(Context context) {
+        this.context = context;
+    }
+
+    public interface VolleyResponseListener {
+        void onError(String message);
+
+        void onResponse(String cityID);
+    }
+
+    public void getCityID(String cityName, VolleyResponseListener volleyResponseListener) {
+        String url = QUERY_FOR_CITY_ID + cityName;
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                cityID = "";
+                try {
+                    JSONObject cityInfo = response.getJSONObject(0);
+                    cityID = cityInfo.getString("woeid");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // This worked. But it didn't retrun the id number to MainActivity
+                //Toast.makeText(context, "City ID = " + cityID, Toast.LENGTH_SHORT).show();
+                volleyResponseListener.onResponse(cityID);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Toast.makeText(context, "Something wrong", Toast.LENGTH_SHORT).show();
+                volleyResponseListener.onError("Something wrong");
+            }
+        });
+
+        MySingleton.getInstance(context).addToRequestQueue(request);
+
+        // Returned a NULL. Problem!
+        //return cityID;
+    }
+
+//    public List<WeatherReportModel> getCityForecastByID(String cityID) {
+//
+//    }
+//
+//    public List<WeatherReportModel> getCityForecastByName(String cityName) {
+//
+//    }
+}
+
+
